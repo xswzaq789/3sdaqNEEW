e75f8d7127a2bd7fe294c39f7492586c4f53d69b
park <parkhyun0924@naver.com>
2022-03-17 13:36:52 +0900

    userApp models.py 추가했습니다!

--- 3sdaq-master/web/userApp/models.py
+++ 3sdaq-master/web/userApp/models.py
@@ -7,9 +7,8 @@ class WebUser(models.Model) :
     user_id      = models.TextField(max_length=100)
     user_pwd     = models.TextField(max_length=100)
     user_name    = models.TextField(max_length=100)
-    user_point = models.IntegerField(default=1000)
-    #user_acct = models.CharField(max_length=50, null=True)
-    #user_amt = models.IntegerField(default=1000000000)
+    user_acct = models.CharField(max_length=50, null=True)
+    user_amt = models.IntegerField(default=1000000000)
     user_regdate = models.DateTimeField(auto_now=True)
 
 class SBS(models.Model):
